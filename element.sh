4a8389e8054de560a19ef4f8b32a022d0b65d4be
KumaCatz K <tod.kagan@gmail.com>
2024-06-20 17:00:30 +0000

    test: element-search-by-atomic-number

--- element.sh
+++ element.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 MAIN_MENU() {
   if [[ -z $1 ]]
@@ -8,6 +8,8 @@ MAIN_MENU() {
     echo Please provide an element as an argument.
   else
     #find element in database
+    ELEMENT_BY_ATOMIC_NUMBER=$($PSQL "SELECT * FROM properties INNER JOIN elements ON properties.atomic_number = elements.atomic_number INNER JOIN types ON properties.type_id = types.type_id WHERE atomic_number = $1")
+    echo "$ELEMENT_BY_ATOMIC_NUMBER"
   fi
 }
 
